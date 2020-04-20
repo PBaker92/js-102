c62c0598fc577b7e571991ce48f4d073129dd85f
Princeton Baker <princeton.bake10@gmail.com>
2020-04-20 14:15:43 -0400

    the function this time

--- printnumbers.js
+++ printnumbers.js
@@ -1,9 +1,14 @@
 // i need a function that has a start number and a end number that prints
 // one number to the other on different lines
-// const startNumber = 0;
-// const endNumber = 10;
+const startNumber = 0;
+const endNumber = 10;
 
-let count = 0;
-for (let count = 0; count<=10; count ++) {
-    console.log(count)
+// let count = 0;
+// for (let count = 0; count<=30; count ++) {
+//     console.log(count)
+// }
+function printNumbers(){
+    for (let count = startNumber; count<= endNumber; count ++)
+     console.log(count)
 }
+printNumbers()
